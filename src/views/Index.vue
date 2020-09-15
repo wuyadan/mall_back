44c8d0a08df5c4b9f0d46d84f620136d1efff6ee
wuyadan <wuyadan007@qq.com>
2020-09-15 23:43:37 +0800

    第一次提交

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>后台首页</div>
+</template>
+<script>
+export default {
+    data(){
+        return{  }
+    },
+    created(){
+      console.log(this.$router)
+    },
+    methods:{},
+    components:{}
+}
+</script>
+<style scoped>
+</style>
